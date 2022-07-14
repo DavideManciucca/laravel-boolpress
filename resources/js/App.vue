1a3f954c82dacff9190ea4f1e0e3e2089e4eec59
DavideManciucca <davidemanciucca@gmail.com>
2022-07-14 17:19:30 +0200

    Apertura repo

---
+++ resources/js/App.vue
@@ -0,0 +1,55 @@
+<template>
+<div>
+    <HeaderComp />
+    <div>
+        <ul>
+            <li v-for="post in posts" :key="post.id">
+            <div> {{post.title}}</div>
+                <div> {{post.content}}</div>
+            </li>
+
+        </ul>
+    </div>
+    <FooterComp />
+</div>
+</template>
+
+<script>
+import HeaderComp from './components/partials/HeaderComp.vue';
+import FooterComp from './components/partials/FooterComp.vue';
+export default {
+    name : 'App',
+    components:{
+        HeaderComp,
+        FooterComp,
+    },
+    data(){
+        return{
+            apiUrl:'/api/posts',
+            posts : null
+        }
+    },
+
+    methods:{
+         getApi(){
+            axios.get(this.apiUrl)
+            .then(res=>{
+             console.log(res.data);
+             this.posts=res.data.posts;
+            //  console.log(this.posts);
+
+            })
+
+         },
+
+    },
+     mounted(){
+         this.getApi()
+
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/components/partials/HeaderComp.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>h</div>
+</template>
+
+<script>
+export default {
+    name:'HeaderComp'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
